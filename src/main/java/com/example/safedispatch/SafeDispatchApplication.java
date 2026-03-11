dc642b28a0a26ceaa94b33840192a3a44a47d595
gdsgrgd452 <bobharrison2005@gmail.com>
2026-03-11 12:47:06 +0000

    Initial commit

---
+++ src/main/java/com/example/safedispatch/SafeDispatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.safedispatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeDispatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeDispatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/safedispatch/SafeDispatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.safedispatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeDispatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
